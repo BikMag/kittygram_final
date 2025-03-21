983bc5e7ff18d7083245e4662c6c5d831a3eefa1
Denis Bikmaev <dbikmaev2003@gmail.com>
2025-03-21 20:41:38 +0300

    Correct images in docker-compose.production.yml and allowed hosts in setting.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", ""),
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.105.191']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
